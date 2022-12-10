33ae0507be53094292f348c8112255221e7e17b0
MdIbrahimKholil7 <kholilmdibrahim17@gmail.com>
2022-12-10 19:47:41 +0600

    message ctrl add

--- controller/messageController.js
+++ controller/messageController.js
@@ -10,7 +10,7 @@ const messageController = {
             const { receiverId, message } = req.body
             const result = await Message.create({
                 sender: req.decoded.id,
-                receiver:receiverId,
+                receiver: receiverId,
                 message,
 
             })
@@ -22,6 +22,28 @@ const messageController = {
                 })
             }
         } catch (error) {
+            console.log(error)
+        }
+    },
+    getMessages: async (req, res) => {
+        try {
+            const { friendId } = req.params
+            const ownId = req.decoded.id
+
+            const result = await Message.find()
+            const filterResult=result.filter(msg=>(msg.sender === ownId && msg.receiver===friendId) || (msg.receiver === ownId && msg.sender===friendId))
+            console.log(filterResult)
+            res.send({
+                status: true,
+                message: "Success",
+                data: filterResult
+            })
+            // console.log(result)
+        } catch (error) {
+            res.status(500).send({
+                status: false,
+                message: "Internal Server Error"
+            })
             console.log(error)
         }
     }
--- route/messageRoutes.js
+++ route/messageRoutes.js
@@ -5,5 +5,6 @@ const router = express.Router()
 
 
 router.post('/addMessage', verifyJwt, messageController.addMessage)
+router.get('/getMessages/:friendId', verifyJwt, messageController.getMessages)
 
 module.exports = router
